5af3d3c77e7ebc65d7f4b46dc2d28964941c11b3
Eliott Ramirez <eliottrg@gmail.com>
2017-10-17 17:02:44 -0500

    Menu aislado en initMenu().

--- tablaSimbolos.c
+++ tablaSimbolos.c
@@ -17,6 +17,8 @@ typedef struct structNodo {
     struct structNodo * next;
 } Nodo;
 
+void initMenu();
+
 Nodo * initNodo(char tipo[], char nombre[], int intvalor, float floatvalor, char charvalor);
 void insertNodo(Nodo ** lista, Nodo * newNodo);
 int deleteNodo(Nodo ** lista, char nombre[]);
@@ -37,115 +39,119 @@ void printTablaHash(Nodo * tablaHash[]);
 Nodo * tablaHash[SIZE];
 
 int main(){
-    int menu = 0;
+    initMenu();
+}
 
-    for (int i = 0; i < SIZE; i++){
-        tablaHash[i] = NULL;
-    }    
+void initMenu(){
+    int menu = 0;
     
-    while (menu != 6){
+        for (int i = 0; i < SIZE; i++){
+            tablaHash[i] = NULL;
+        }    
         
-        if (menu == 0){
-            printf("\n¿Qué desea hacer?:\n\
-                   1. Dar de alta una variable.\n\
-                   2. Dar de baja una variable.\n\
-                   3. Buscar una variable.\n\
-                   4. Imprimir la tabla de variables.\n\
-                   5. Limpiar la lista tabla de variables.\n\
-                   6. Salir.\n");
+        while (menu != 6){
             
-            scanf("%d", &menu);
-        }
-        else if (menu == 1){
-            
-            char tipo[10];
-            char nombre[40];
-            int intvalor;
-            float floatvalor;
-            char charvalor;
-            
-            printf("Proporcione tipo de la variable: \n");
-            scanf("%s", tipo);
-            
-            if ((strcmp("int", tipo) != 0) && (strcmp("float", tipo) != 0) && (strcmp("char", tipo) != 0)){
-                printf("Tipo inválido.\n");
-                menu = 0;
+            if (menu == 0){
+                printf("\n¿Qué desea hacer?:\n\
+                       1. Dar de alta una variable.\n\
+                       2. Dar de baja una variable.\n\
+                       3. Buscar una variable.\n\
+                       4. Imprimir la tabla de variables.\n\
+                       5. Limpiar la lista tabla de variables.\n\
+                       6. Salir.\n");
+                
+                scanf("%d", &menu);
             }
-            else {
-                printf("Proporcione nombre de la variable: \n");
+            else if (menu == 1){
+                
+                char tipo[10];
+                char nombre[40];
+                int intvalor;
+                float floatvalor;
+                char charvalor;
+                
+                printf("Proporcione tipo de la variable: \n");
+                scanf("%s", tipo);
+                
+                if ((strcmp("int", tipo) != 0) && (strcmp("float", tipo) != 0) && (strcmp("char", tipo) != 0)){
+                    printf("Tipo inválido.\n");
+                    menu = 0;
+                }
+                else {
+                    printf("Proporcione nombre de la variable: \n");
+                    scanf("%s", nombre);
+                    
+                    printf("Proporcione valor de la variable: \n");
+                    if (strcmp("int", tipo) == 0){
+                        scanf("%d", &intvalor);
+                        floatvalor = 0;
+                        charvalor = '\0';
+                    }
+                    else if (strcmp("float", tipo) == 0){
+                        intvalor = 0;
+                        scanf("%f", &floatvalor);
+                        charvalor = '\0';
+                    }
+                    else if (strcmp("char", tipo) == 0){
+                        intvalor = 0;
+                        floatvalor = 0;
+                        scanf(" %c", &charvalor);
+                    }
+                    
+                    insert(tablaHash, initNodo(tipo, nombre, intvalor, floatvalor, charvalor));
+                    menu = 0;
+                }
+            }
+            else if (menu == 2){
+                char nombre[40];
+                int found = 0;
+                
+                printf("Proporcione el nombre de la variable a eliminar: \n");
                 scanf("%s", nombre);
                 
-                printf("Proporcione valor de la variable: \n");
-                if (strcmp("int", tipo) == 0){
-                    scanf("%d", &intvalor);
-                    floatvalor = 0;
-                    charvalor = '\0';
+                found = delete(tablaHash, nombre);
+                
+                if (found == 1){
+                    printf("Variable encontrada y eliminada.\n");
                 }
-                else if (strcmp("float", tipo) == 0){
-                    intvalor = 0;
-                    scanf("%f", &floatvalor);
-                    charvalor = '\0';
-                }
-                else if (strcmp("char", tipo) == 0){
-                    intvalor = 0;
-                    floatvalor = 0;
-                    scanf(" %c", &charvalor);
+                else if (found == -1){
+                    printf("Variable no encontrada.\n");
                 }
                 
-                insert(tablaHash, initNodo(tipo, nombre, intvalor, floatvalor, charvalor));
+                menu = 0;
+            }
+            else if (menu == 3){
+                char nombre[40];
+                Nodo * found;
+    
+                printf("Proporcione el nombre de la variable a buscar: \n");
+                scanf("%s", nombre);
+                
+                found = search(tablaHash, nombre);
+                
+                if (found != NULL){
+                    printf("Variable encontrada:\n");
+                    printNodo(found);
+                }
+                else {
+                    printf("Variable no encontrada.\n");
+                }
+                
+                menu = 0;
+            }
+            else if (menu == 4){
+                printTablaHash(tablaHash);
+                menu = 0;
+            }
+            else if (menu == 5){
+                purgeTablaHash(tablaHash);
+                menu = 0;
+            }
+            else {
+                printf("Opción inválida.\n");
                 menu = 0;
             }
         }
-        else if (menu == 2){
-            char nombre[40];
-            int found = 0;
-            
-            printf("Proporcione el nombre de la variable a eliminar: \n");
-            scanf("%s", nombre);
-            
-            found = delete(tablaHash, nombre);
-            
-            if (found == 1){
-                printf("Variable encontrada y eliminada.\n");
-            }
-            else if (found == -1){
-                printf("Variable no encontrada.\n");
-            }
-            
-            menu = 0;
-        }
-        else if (menu == 3){
-            char nombre[40];
-            Nodo * found;
-
-            printf("Proporcione el nombre de la variable a buscar: \n");
-            scanf("%s", nombre);
-            
-            found = search(tablaHash, nombre);
-            
-            if (found != NULL){
-                printf("Variable encontrada:\n");
-                printNodo(found);
-            }
-            else {
-                printf("Variable no encontrada.\n");
-            }
-            
-            menu = 0;
-        }
-        else if (menu == 4){
-            printTablaHash(tablaHash);
-            menu = 0;
-        }
-        else if (menu == 5){
-            purgeTablaHash(tablaHash);
-            menu = 0;
-        }
-        else {
-            printf("Opción inválida.\n");
-            menu = 0;
-        }
-    }
 }
 
 int fHash(char nombre[]){
